3c8a65f6b77a238575e59bfff5a191aa2c3a4c75
Clyde-Kipkoech <158745563+Clyde-kipkoech@users.noreply.github.com>
2025-04-27 21:09:06 +0300

    final commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function hireMe() {
-    // Example: redirect to contact page or open email client
-    window.location.href = "mailto: kipkoechclyde546@gm"; // Replace with your email
+    
+    window.location.href = "mailto: kipkoechclyde546@gm"; 
   }
 
   function downloadCV() {
-    // Example: download CV from a file
+    
     const link = document.createElement('a');
-    link.href = 'assets/cv.pdf';  // Replace with your CV file path
-    link.download = 'Clyde_Kipkoech_CV.pdf';  // File name when downloading
+    link.href = 'assets/cv.pdf';
+    link.download = 'Clyde_Kipkoech_CV.pdf';  
     link.click();
   }
